b968f288b4d0917f7968f6b0eb8e01d91a64944c
victordhn <33136394+victordhn@users.noreply.github.com>
2024-06-12 23:09:49 -0300

    update

--- training.sql
+++ training.sql
@@ -22,3 +22,23 @@ CREATE TABLE march_jobs AS
 
 SELECT *
     FROM january_jobs
+
+-- CASE EXPRESSION
+
+SELECT
+    job_title_short,
+    job_location
+FROM job_postings_fact
+
+SELECT
+    COUNT(job_id),
+    CASE
+        WHEN job_location = 'Anywhere' THEN 'Remote'
+        WHEN job_location = 'New York, NY' THEN 'Local'
+        ELSE 'Onsite' 
+    END AS new_location
+FROM job_postings_fact
+WHERE job_title_short = 'Data Analyst'
+GROUP BY new_location
+
+-- continue at 2:30:27
