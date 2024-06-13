20118c36651890558af823caa1589422a966a012
victordhn <33136394+victordhn@users.noreply.github.com>
2024-06-13 02:52:42 -0300

    update

--- training.sql
+++ training.sql
@@ -1,3 +1,5 @@
+-- A bunch of code to practice
+
 SELECT 
     job_title_short as title,
     job_location as location,
@@ -42,3 +44,52 @@ WHERE job_title_short = 'Data Analyst'
 GROUP BY new_location
 
 -- continue at 2:30:27
+
+-- subqueries
+
+SELECT *
+FROM (
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date) = 1
+) AS january_jobs2
+
+WITH january_jobs2 AS (
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date) = 1
+)
+
+SELECT *
+FROM january_jobs2
+
+SELECT * 
+FROM (
+    SELECT *
+    FROM job_postings_fact
+    WHERE job_no_degree_mention = false
+) AS jobs_nodegree
+
+
+WITH nodegree AS (
+    SELECT *
+    FROM job_postings_fact
+    WHERE job_no_degree_mention = false
+)
+
+SELECT
+    nodegree.job_title_short,
+    company_dim.name,
+    nodegree.job_no_degree_mention
+    FROM nodegree
+    RIGHT JOIN company_dim
+    ON nodegree.company_id = company_dim.company_id
+
+SELECT name AS name
+FROM company_dim
+WHERE company_id IN (
+    SELECT
+        company_id
+    FROM job_postings_fact 
+    WHERE job_no_degree_mention = false
+)
---
+++ training_2.sql
@@ -0,0 +1,47 @@
+
+
+SELECT 
+    company_dim.name,
+    COUNT(job_postings_fact.job_id) AS jobs
+FROM company_dim
+LEFT JOIN job_postings_fact
+ON company_dim.company_id = job_postings_fact.company_id
+GROUP BY company_dim.name
+ORDER BY jobs DESC
+
+
+SELECT
+    skills_dim.skill_id,
+    skills_dim.skills,
+    COUNT(job_postings_fact.job_id) AS jobs
+FROM skills_dim
+LEFT JOIN skills_job_dim ON skills_dim.skill_id = skills_job_dim.skill_id
+LEFT JOIN job_postings_fact ON skills_job_dim.job_id = job_postings_fact.job_id
+WHERE job_postings_fact.job_work_from_home = true AND
+        job_postings_fact.job_title_short = 'Data Analyst'
+GROUP BY skills_dim.skills, skills_dim.skill_id
+ORDER BY jobs DESC
+LIMIT 5
+
+-- CTE
+WITH remote_job_skills AS (
+    SELECT
+        COUNT(skills_to_jobs.job_id) AS jobs,
+        skills_to_jobs.skill_id
+    FROM skills_job_dim as skills_to_jobs
+    INNER JOIN job_postings_fact AS job_postings
+        ON skills_to_jobs.job_id = job_postings.job_id
+    WHERE job_postings.job_work_from_home = true AND 
+        job_postings.job_title_short = 'Data Analyst'
+    GROUP BY skills_to_jobs.skill_id
+)
+
+SELECT
+    remote_job_skills.jobs,
+    skills_dim.skills
+FROM remote_job_skills 
+INNER JOIN skills_dim ON remote_job_skills.skill_id = skills_dim.skill_id
+ORDER BY jobs DESC
+LIMIT 5
+
+-- continue 2:50:17
