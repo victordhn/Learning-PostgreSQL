039815c5f4de35566a2f35d17d3d1048fe2eafd8
victordhn <33136394+victordhn@users.noreply.github.com>
2024-06-13 18:12:08 -0300

    update

--- SQL training/training_2.sql
+++ SQL training/training_2.sql
@@ -44,4 +44,19 @@ INNER JOIN skills_dim ON remote_job_skills.skill_id = skills_dim.skill_id
 ORDER BY jobs DESC
 LIMIT 5
 
--- continue 2:50:17
+-- UNION
+CREATE TABLE february_jobs AS(
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date) = 2
+)
+
+SELECT *
+FROM (
+    SELECT *
+    FROM january_jobs
+    UNION
+    SELECT *
+    FROM february_jobs)
+WHERE salary_year_avg > 70000 AND job_title_short = 'Data Analyst'
+ORDER BY salary_year_avg
--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 # PostgreSQL Learning
 * Following course by [Luke Barousse](https://www.youtube.com/watch?v=7mz73uXD9DA&t=7376s)
 * Data is available on this Github and on [Luke's website]( https://lukebarousse.com/sql)
-* Progress: 60%
+* Progress: 70%
+* For extra training, check these PostgreSQL exercises: https://pgexercises.com/
+
+## Proposed exercises
